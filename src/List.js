242c253c9eb66195a185bcecc81b5bbbd004f5be
JoelMaciel <soldado25461@gmail.com>
2021-02-10 23:42:11 -0300

    Apagando registros

--- src/List.js
+++ src/List.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 
 function List(props) {
+
+    
     
 
     return(<ul>
-        {props.items.map(item =><li key={item.id}>{item.text}</li>)}
+        {props.items.map(item =><li key={item.id}>
+            {item.text}
+            <button onClick = {( )=>{props.onItemDeleted(item)}}><img alt="delete" src="./bin.jpg" width = "30px"></img></button>
+            </li>)}
 
     </ul>);
 }
